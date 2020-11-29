ac8d3a2bb0dfed2f4936d340a7d80fd4f8c08220
MichalMajb <60367341+MichalMajb@users.noreply.github.com>
2020-11-29 17:37:41 +0100

    Add files via upload

---
+++ P6/P6/P6.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "stdlib.h"
+#include <iostream>
+
+void main() {
+	int * tab = nullptr, size = 0;
+	std::cout << "Podawaj liczby, 0 konczy wczytywanie.\n";
+	int *newTab = new int[size];
+	while (true) {
+		int number;
+		std::cin >> number;
+
+		if (number == 0) {
+			break;
+		}
+	/*	for (int i = 0; i < size; ++i) {
+			newTab[i] = tab[i];
+		} */
+		newTab[size] = number;
+		//delete[] tab;
+		//tab = newTab;
+		size++;
+	}
+	std::cout << "to samo ale odwrotnie \n";
+	for (int i = size - 1; i >= 0; i--) {
+		std::cout << newTab[i] << ' ';
+	}
+	delete [size] newTab;
+}
